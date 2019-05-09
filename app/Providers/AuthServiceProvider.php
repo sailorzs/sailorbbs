cba861be9f4b5db2cf0dd29b400b3c0288b68d1c
sailorzs <sailorzs@126.com>
2019-05-09 05:53:31 +0000

    授权访问

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        'App\Model\User' => 'App\Policies\UserPolicy'
+
     ];
 
     /**
@@ -24,7 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        // 修改策略自动发现的逻辑
+        //Gate::guessPolicyNamesUsing(function ($modelClass) {
+            // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
+        //    return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        //});
     }
 }
