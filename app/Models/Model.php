9bf22f5d0ce77ec7595e67d9fd873b0cb9899d8f
sailorzs <sailorzs@126.com>
2019-05-16 13:31:03 +0000

    回复列表

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel
 {
-    //public function scopeRecent($query)
-    //{
-    //    return $query->orderBy('id', 'desc');
-    //}
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('id', 'desc');
+    }
 
     public function scopeOrdered($query)
     {
